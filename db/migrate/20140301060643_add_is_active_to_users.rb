5446c0f0f8396c518a804285880726aa621d4b83
Erik Michaels-Ober <sferik@gmail.com>
2014-03-04 19:33:50 +0530

    version migration

--- db/migrate/20140301060643_add_is_active_to_users.rb
+++ db/migrate/20140301060643_add_is_active_to_users.rb
@@ -1,6 +1,6 @@
 class AddIsActiveToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :status, :boolean,:default => 1
-    add_column :users, :fullname, :string,:default => 1
+    add_column :users, :status, :boolean,:default => 'true'
+    add_column :users, :fullname, :string,:default => 'true'
   end
 end
