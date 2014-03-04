3587f4fd98df72d6cb01fc52fe473d1938764a4f
Erik Michaels-Ober <sferik@gmail.com>
2014-03-04 20:02:20 +0530

    boolen value changed

--- db/migrate/20140301060643_add_is_active_to_users.rb
+++ db/migrate/20140301060643_add_is_active_to_users.rb
@@ -1,6 +1,6 @@
 class AddIsActiveToUsers < ActiveRecord::Migration
   def change
     add_column :users, :status, :boolean,:default => 'true'
-    add_column :users, :fullname, :string,:default => 'true'
+    add_column :users, :fullname, :string
   end
 end
