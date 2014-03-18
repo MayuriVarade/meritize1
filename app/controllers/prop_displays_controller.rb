08f5e33dc3474995e630803cd797c25dd2c04484
kipl <kipl@kipl-Inspiron-620s.(none)>
2014-03-18 11:18:55 +0530

    design integration in prop display page

--- app/controllers/prop_displays_controller.rb
+++ app/controllers/prop_displays_controller.rb
@@ -5,11 +5,23 @@ class PropDisplaysController < ApplicationController
   end
 
   def new
+    @prop = current_user.admin_user.prop
+    @prop_displays = PropDisplay.all
   	@prop_display = PropDisplay.new
+     @searchuser ||= [] 
+        @adminusers = User.where(["firstname || lastname || fullname LIKE ? and admin_user_id is not null", "%#{params[:search]}%"])
+        @adminusers.each do |adminuser|
+        fullname = adminuser.fullname
+        @searchuser << fullname
+       end
+       @searchuser
   end
 
   def create
+    receiver = params[:prop_display][:receiver_id]
+   receiver_id = User.find_by_fullname(receiver).id
   	@prop_display = PropDisplay.new(params[:prop_display])
+    @prop_display.receiver_id = receiver_id
   	if @prop_display.save
   		redirect_to :back
   	end  	
