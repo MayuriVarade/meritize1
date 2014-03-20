47539154ff72db894c7bb4cbdc969483fa2c039a
kipl <kipl@kipl-Inspiron-620s.(none)>
2014-03-20 17:17:42 +0530

    Account creation email text changes ,Feedback about the Settings  Props screen,Feedback about the Settings Vote screen,Change message after account creation,User activities screen changes,Change text of welcome email sent to new user (not administrator)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,7 @@ class UsersController < ApplicationController
    before_filter :authenticate, :only => [:edit, :update,:dashboard]
    before_filter :correct_user, :only => [:show]
    before_filter :correct_user_edit, :only => [:edit,:update]
-   before_filter :skip_password_attribute, only: :update
    layout :custom_layout
-   require 'will_paginate/array'
 
   def show
     @user = User.find(params[:id])
@@ -55,9 +53,8 @@ class UsersController < ApplicationController
 
    def adminuser_logs
        @searchuser ||= [] 
-
-       @adminusers = AdminuserLog.find_all_by_admin_user_id(current_user.id, :conditions => ["firstname || lastname || fullname LIKE ?", "%#{params[:search]}%"]).paginate :page => params[:page],:per_page => 10
-
+        @users = User.all
+       @adminusers = AdminuserLog.find_all_by_admin_user_id(current_user.id, :conditions => ["firstname || lastname || fullname LIKE ?", "%#{params[:search]}%"])
        @adminusers.each do |adminuser|
         fullname = adminuser.fullname
         @searchuser << fullname
@@ -184,12 +181,6 @@ class UsersController < ApplicationController
  #          format.json { render :json => !@user }
  #     end
  #  end
-    def skip_password_attribute
-    if params[:password].blank? && params[:password_validation].blank?
-      params.except!(:password, :password_validation)
-    end
-  end
-
 
     def assign_password
       (0..6).map{ ('a'..'z').to_a[rand(26)] }.join
