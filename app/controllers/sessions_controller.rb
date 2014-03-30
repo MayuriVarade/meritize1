a8afcfcc407f2f7b0f2d31fbf212bf73e62de489
railskipl <rails@kunalinfotech.net>
2014-03-30 12:34:17 +0530

    changes in subscription and productmanagerlogs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,19 +20,32 @@ class SessionsController < ApplicationController
        elsif user.username == "productadmin"
           sign_in user
           redirect_to user_root_path
-         # if user.admin_user.status.present?
-         elsif (user.sign_in_count == 1) && ((user.username.nil?) && (user.admin_user_id.nil?))
-          sign_in user
-          redirect_to change_password_path
-           
-         elsif (user.sign_in_count > 1) && ((user.username.nil?) && (user.admin_user_id.nil?)) 
-          sign_in user
-          redirect_to user_root_path 
-       
-         elsif user.admin_user.status == false  
-          flash.now[:error] = "Your account has been deactivated"
-          @title = "Sign in"
-          render 'new'
+       elsif (user.username.nil?) && (user.admin_user_id.nil?)
+         sign_in user
+            if params[:remember_me]
+              cookies.permanent[:auth_token] = user.auth_token
+           else
+             cookies[:auth_token] = user.auth_token  
+           end
+           count = current_user.sign_in_count
+           tot_count = count + 1
+           user.update_column(:sign_in_count,tot_count)
+           user.update_column(:last_sign_in,Time.now)
+                
+           if current_user.role?(:admin)
+            ProductManagerLog.create(:user_id =>current_user.id,:admin_user_id => current_user.admin_user_id,:sign_in_time => Time.now,:firstname =>current_user.firstname,:lastname =>current_user.lastname,:fullname => current_user.fullname,:email =>current_user.email,:sign_in_count =>tot_count,:ip_address => request.remote_ip)
+           end 
+
+            if count == 1
+                redirect_to change_password_path
+            else
+              redirect_to user_root_path
+            end
+
+       elsif user.admin_user.status == false
+            flash.now[:error] = "Your account has been deactivated"
+            @title = "Sign in"
+            render 'new'
       else
          sign_in user
          if params[:remember_me]
@@ -47,11 +60,7 @@ class SessionsController < ApplicationController
 
          if current_user.role?(:user)
           AdminuserLog.create(:user_id =>current_user.id,:admin_user_id => current_user.admin_user_id,:sign_in_time => Time.now,:firstname =>current_user.firstname,:lastname =>current_user.lastname,:username => current_user.username,:fullname => current_user.fullname,:email =>current_user.email,:sign_in_count =>tot_count,:ip_address => request.remote_ip)
-       
-         elsif current_user.role?(:admin)
-         
-          ProductManagerLog.create(:user_id =>current_user.id,:admin_user_id => current_user.admin_user_id,:sign_in_time => Time.now,:firstname =>current_user.firstname,:lastname =>current_user.lastname,:fullname => current_user.fullname,:email =>current_user.email,:sign_in_count =>tot_count,:ip_address => request.remote_ip)
-         end 
+         end
 
          if (count == 1) and (current_user.role?(:admin) || current_user.role?(:user))
 
