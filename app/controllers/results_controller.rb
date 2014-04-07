15696e8049c765c2f8cb9ac42aa5d709f95e45fe
railskipl <rails@kunalinfotech.net>
2014-04-07 11:13:43 +0530

    changes in result controller

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -4,11 +4,11 @@ class ResultsController < ApplicationController
   	if request.post? || params[:result]
          @vote_setting1 = current_user.vote_setting 
   	     @vote_setting = VoteCycle.find_by_id(params[:result][:cycle])
-  	     @result = VoteCount.where("start_cycle = '#{@vote_setting.start_cycle.to_date}' AND end_cycle ='#{@vote_setting.end_cycle.to_date}' AND vote_count >0").order('vote_count DESC').limit(10) rescue nil
+  	     @result = VoteCount.where("start_cycle = '#{@vote_setting.start_cycle}' AND end_cycle ='#{@vote_setting.end_cycle}' AND vote_count >0").order('vote_count DESC').limit(10) rescue nil
   	else
   	  @vote_setting1 = current_user.vote_setting 
   	  @current_result = Result.find_by_start_cycle_and_end_cycle(@vote_setting1.start_cycle,@vote_setting1.end_cycle) rescue nil
-  	  @result = VoteCount.where("start_cycle = '#{@vote_setting1.start_cycle.to_date}' AND end_cycle ='#{@vote_setting1.end_cycle.to_date}' AND vote_count > 0 ").order('vote_count DESC').limit(10) rescue nil
+  	  @result = VoteCount.where("start_cycle = '#{@vote_setting1.start_cycle}' AND end_cycle ='#{@vote_setting1.end_cycle}' AND vote_count > 0 ").order('vote_count DESC').limit(10) rescue nil
   	end	
   end
 
