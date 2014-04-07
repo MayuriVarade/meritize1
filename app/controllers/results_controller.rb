d467ed9b1ef310bca231248af3c6049234d7ebf7
railskipl <rails@kunalinfotech.net>
2014-04-07 11:47:01 +0530

    changes in prop_display helper

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -4,11 +4,11 @@ class ResultsController < ApplicationController
   	if request.post? || params[:result]
          @vote_setting1 = current_user.vote_setting 
   	     @vote_setting = VoteCycle.find_by_id(params[:result][:cycle])
-  	     @result = VoteCount.where("start_cycle = '#{@vote_setting.start_cycle.to_date}' AND end_cycle ='#{@vote_setting.end_cycle.to_date}' AND vote_count >0").order('vote_count DESC').limit(10) rescue nil
+  	     @result = VoteCount.where("start_cycle = '#{@vote_setting.start_cycle}' AND end_cycle ='#{@vote_setting.end_cycle}' AND vote_count >0").order('vote_count DESC').limit(10) rescue nil
   	else
   	  @vote_setting1 = current_user.vote_setting 
   	  @current_result = Result.find_by_start_cycle_and_end_cycle(@vote_setting1.start_cycle,@vote_setting1.end_cycle) rescue nil
-  	  @result = VoteCount.where("start_cycle = '#{@vote_setting1.start_cycle.to_date}' AND end_cycle ='#{@vote_setting1.end_cycle.to_date}' AND vote_count > 0 ").order('vote_count DESC').limit(10) rescue nil
+  	  @result = VoteCount.where("start_cycle = '#{@vote_setting1.start_cycle}' AND end_cycle ='#{@vote_setting1.end_cycle}' AND vote_count > 0 ").order('vote_count DESC').limit(10) rescue nil
   	end	
   end
 
@@ -22,15 +22,15 @@ class ResultsController < ApplicationController
     if request.post? || params[:result]
          @prop_setting = current_user.prop 
          @prop_setting1 = PropCycle.find_by_id(params[:result][:cycle])
-         @result = PropCount.where("start_cycle = '#{@prop_setting1.start_cycle.to_date}' AND end_cycle ='#{@prop_setting1.end_cycle.to_date}' AND prop_count >0").order('prop_count DESC').limit(10) rescue nil
+         @result = PropCount.where("start_cycle = '#{@prop_setting1.start_cycle}' AND end_cycle ='#{@prop_setting1.end_cycle}' AND prop_count >0").order('prop_count DESC').limit(10) rescue nil
 
-         @result_by_points = PropCount.where("start_cycle = '#{@prop_setting1.start_cycle.to_date}' AND end_cycle ='#{@prop_setting1.end_cycle.to_date}' AND points > 0").order('points DESC').limit(10) rescue nil
+         @result_by_points = PropCount.where("start_cycle = '#{@prop_setting1.start_cycle}' AND end_cycle ='#{@prop_setting1.end_cycle}' AND points > 0").order('points DESC').limit(10) rescue nil
     else
       @prop_setting = current_user.prop 
       # @current_result = Result.find_by_start_cycle_and_end_cycle(@vote_setting1.start_cycle,@vote_setting1.end_cycle) rescue nil
-      @result = PropCount.where("start_cycle = '#{@prop_setting.start_cycle.to_date}' AND end_cycle ='#{@prop_setting.end_cycle.to_date}' AND prop_count > 0 ").order('prop_count DESC').limit(10) rescue nil
+      @result = PropCount.where("start_cycle = '#{@prop_setting.start_cycle}' AND end_cycle ='#{@prop_setting.end_cycle}' AND prop_count > 0 ").order('prop_count DESC').limit(10) rescue nil
 
-       @result_by_points = PropCount.where("start_cycle = '#{@prop_setting.start_cycle.to_date}' AND end_cycle ='#{@prop_setting.end_cycle.to_date}' AND points > 0").order('points DESC').limit(10) rescue nil
+       @result_by_points = PropCount.where("start_cycle = '#{@prop_setting.start_cycle}' AND end_cycle ='#{@prop_setting.end_cycle}' AND points > 0").order('points DESC').limit(10) rescue nil
     end 
   end
 end
