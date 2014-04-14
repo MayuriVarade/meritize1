00fef33657e496d87fb897aee9ae779536757e2a
railskipl <rails@kunalinfotech.net>
2014-04-14 18:45:21 +0530

    changes in votes helper

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -13,14 +13,14 @@ module VotesHelper
     if @vote.voteable.department == @vote.voter.department 
        if @vote_count.empty?
        
-          @voter_count = VoteCount.create(:voteable_id => @receiver ,:start_cycle =>@vote_setting1.start_cycle,:end_cycle => @vote_setting1.end_cycle,:vote_count => @count,:user_id => current_user.admin_user.id) rescue nil
+          @voter_count = VoteCount.create(:voteable_id => @receiver ,:start_cycle =>@vote_setting1.start_cycle,:end_cycle => @vote_setting1.end_cycle,:vote_count => 1,:user_id => current_user.admin_user.id) rescue nil
        else
        	@update_count = VoteCount.update(@voteable.id,:voteable_id => @receiver ,:start_cycle =>@voteable.start_cycle,:end_cycle => @voteable.end_cycle,:vote_count => @count,:user_id => current_user.admin_user.id) rescue nil 
        end
     else
        if @vote_other.empty? && @vote_count.empty?
            
-         @voter_count1 = VoteCount.create(:voteable_id => @receiver ,:start_cycle =>@vote_setting1.start_cycle,:end_cycle => @vote_setting1.end_cycle,:vote_count => @count,:user_id => current_user.admin_user.id) rescue nil
+         @voter_count1 = VoteCount.create(:voteable_id => @receiver ,:start_cycle =>@vote_setting1.start_cycle,:end_cycle => @vote_setting1.end_cycle,:vote_count => 1,:user_id => current_user.admin_user.id) rescue nil
           @voter_count = VoteOtherCount.create(:voteable_id => @receiver ,:start_cycle =>@vote_setting1.start_cycle,:end_cycle => @vote_setting1.end_cycle,:vote_count => @count,:user_id => current_user.admin_user.id) rescue nil
        else
           @update_count1 = VoteCount.update(@voteable.id,:voteable_id => @receiver ,:start_cycle =>@voteable.start_cycle,:end_cycle => @voteable.end_cycle,:vote_count => @count,:user_id => current_user.admin_user.id) rescue nil 
