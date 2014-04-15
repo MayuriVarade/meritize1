15748738dd8baf056722f7d057ad139487578e6d
railskipl <rails@kunalinfotech.net>
2014-04-15 17:51:06 +0530

    changes in wows

--- db/migrate/20140313055610_create_prop_cycles.rb
+++ db/migrate/20140313055610_create_prop_cycles.rb
@@ -1,8 +1,8 @@
 class CreatePropCycles < ActiveRecord::Migration
   def change
     create_table :prop_cycles do |t|
-      t.date :start_cycle
-      t.date :end_cycle
+      t.datetime :start_cycle
+      t.datetime :end_cycle
       t.integer :user_id
       t.integer :prop_id
 
