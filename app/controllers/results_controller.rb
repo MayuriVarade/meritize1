c96798d080bbefabe1306619987ee62fbd435c0a
railskipl <rails@kunalinfotech.net>
2014-04-17 12:45:22 +0530

    trello changes apr 17

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -79,7 +79,7 @@ class ResultsController < ApplicationController
          @diff = (@prop_setting_end - @prop_setting_start + 1).round rescue nil
          @prop_setting1 = PropCycle.find_by_id(params[:result][:cycle]) rescue nil
          @current_result = PropResult.find_by_start_cycle_and_end_cycle_and_user_id(@prop_setting.start_cycle,@prop_setting.end_cycle,current_user.id) rescue nil
-         @select_winner_month = (@prop_setting.end_cycle.to_date - 7) rescue nil
+         @select_winner_month = (@prop_setting.end_cycle.to_date - 100) rescue nil
          @todays_date = Date.today
          @result = PropCount.where("start_cycle = '#{@prop_setting1.start_cycle.to_date}' AND end_cycle ='#{@prop_setting1.end_cycle.to_date}' AND prop_count >0 AND user_id = '#{current_user.id}'").order('prop_count DESC').limit(10) rescue nil
          @result_by_points = PropCount.where("start_cycle = '#{@prop_setting1.start_cycle.to_date}' AND end_cycle ='#{@prop_setting1.end_cycle.to_date}' AND points > 0 AND user_id = '#{current_user.id}'").order('points DESC').limit(10) rescue nil
@@ -91,7 +91,7 @@ class ResultsController < ApplicationController
           @diff = (@prop_setting_end - @prop_setting_start + 1).round rescue nil
 
           @current_result = PropResult.find_by_start_cycle_and_end_cycle_and_user_id(@prop_setting.start_cycle,@prop_setting.end_cycle,current_user.id) rescue nil
-          @select_winner_month = (@prop_setting.end_cycle.to_date - 7) rescue nil
+          @select_winner_month = (@prop_setting.end_cycle.to_date - 100) rescue nil
           @todays_date = Date.today
           # @current_result = Result.find_by_start_cycle_and_end_cycle(@vote_setting1.start_cycle,@vote_setting1.end_cycle) rescue nil
           @result = PropCount.where("start_cycle = '#{@prop_setting.start_cycle.to_date}' AND end_cycle ='#{@prop_setting.end_cycle.to_date}' AND prop_count > 0 AND user_id = '#{current_user.id}'").order('prop_count DESC').limit(10) rescue nil
@@ -108,7 +108,7 @@ class ResultsController < ApplicationController
           @diff = (@prop_setting_end - @prop_setting_start + 1).round rescue nil
 
           @current_result = PropResult.find_by_start_cycle_and_end_cycle_and_user_id(@prop_setting.start_cycle,@prop_setting.end_cycle,current_user.id) rescue nil
-          @select_winner_month = (@prop_setting.end_cycle.to_date - 7) rescue nil
+          @select_winner_month = (@prop_setting.end_cycle.to_date - 100) rescue nil
           @todays_date = Date.today 
       # @current_result = Result.find_by_start_cycle_and_end_cycle(@vote_setting1.start_cycle,@vote_setting1.end_cycle) rescue nil
       @result = PropCount.where("start_cycle = '#{@prop_setting.start_cycle.to_date}' AND end_cycle ='#{@prop_setting.end_cycle.to_date}' AND prop_count > 0 AND user_id = '#{current_user.id}'").order('prop_count DESC').limit(10) rescue nil
