2a92b2f37557eb250185941d975dc5e28f8fdd2d
railskipl <rails@kunalinfotech.net>
2014-04-17 13:04:29 +0530

    trello changes apr 17

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -14,7 +14,7 @@ class ResultsController < ApplicationController
          @current_result = Result.find_by_start_cycle_and_end_cycle_and_user_id(@vote_setting.start_cycle,@vote_setting.end_cycle,current_user.id) rescue nil
          @result = VoteCount.where("start_cycle = '#{@vote_setting.start_cycle.to_date}' AND end_cycle ='#{@vote_setting.end_cycle.to_date}' AND vote_count > 0 AND user_id = '#{current_user.id}'").order('vote_count DESC').limit(10) rescue nil
          @select_winner_month = (@vote_setting1.end_cycle.to_date - 7) rescue nil
-         @select_winner_week = (@vote_setting1.end_cycle.to_date - 1) rescue nil
+         @select_winner_week = (@vote_setting1.end_cycle.to_date - 10) rescue nil
          @todays_date = Date.today
          @result_other_dept = VoteOtherCount.where("start_cycle = '#{@vote_setting.start_cycle.to_date}' AND end_cycle ='#{@vote_setting.end_cycle.to_date}' AND vote_count > 0 AND user_id = '#{current_user.id}'").order('vote_count DESC').limit(10) rescue nil
          @engage_user = EngageUser.where("vote_count > 0 AND user_id = '#{current_user.id}'").order('vote_count DESC').limit(10) rescue nil
@@ -26,7 +26,7 @@ class ResultsController < ApplicationController
         @diff = (@vote_setting_end - @vote_setting_start + 1).round rescue nil
 
         @select_winner_month = (@vote_setting1.end_cycle.to_date - 7) rescue nil
-        @select_winner_week = (@vote_setting1.end_cycle.to_date - 1) rescue nil
+        @select_winner_week = (@vote_setting1.end_cycle.to_date - 10) rescue nil
         @todays_date = Date.today
 
         @current_result = Result.find_by_start_cycle_and_end_cycle_and_user_id(@vote_setting1.start_cycle,@vote_setting1.end_cycle,current_user.id) rescue nil
@@ -43,7 +43,7 @@ class ResultsController < ApplicationController
       @diff = (@vote_setting_end - @vote_setting_start + 1).round rescue nil
 
       @select_winner_month = (@vote_setting1.end_cycle.to_date - 100) rescue nil
-      @select_winner_week = (@vote_setting1.end_cycle.to_date - 1) rescue nil
+      @select_winner_week = (@vote_setting1.end_cycle.to_date - 10) rescue nil
       @todays_date = Date.today
 
   	  @current_result = Result.find_by_start_cycle_and_end_cycle_and_user_id(@vote_setting1.start_cycle,@vote_setting1.end_cycle,current_user.id) rescue nil
