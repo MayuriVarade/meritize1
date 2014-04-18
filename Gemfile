cc807c89f62c700b6cbe85b6af030900038f2a0e
kipl <kipl@kipl-Inspiron-620s.(none)>
2014-04-18 15:13:10 +0530

    User upload issues

--- Gemfile
+++ Gemfile
@@ -11,7 +11,7 @@ gem 'rails', '3.2.14'
 
 # Postgres gem used to create database on server
 
-#gem 'sqlite3'
+gem 'sqlite3'
 gem 'execjs'
 gem 'therubyracer'
 gem 'paperclip'
@@ -22,6 +22,7 @@ gem 'bundler'
 gem "will_paginate"
 gem 'aws-s3'
 gem 'aws-sdk'
+gem 'fastercsv'
 
 gem 'roo'
 
--- app/models/adminuser_log.rb
+++ app/models/adminuser_log.rb
@@ -1,5 +1,5 @@
 class AdminuserLog < ActiveRecord::Base
-   attr_accessible :user_id, :admin_user_id,:sign_in_time,:sign_out_time,:ip_address,:firstname,:lastname,:username,:email,:sign_in_count,:fullname
+   attr_accessible :user_id, :admin_user_id,:sign_in_time,:sign_out_time,:ip_address,:firstname,:lastname,:username,:email,:sign_in_count,:fullname,:department
  
 	def self.to_csv
 	    CSV.generate do |csv|
