d71644f9871729e0e558922e90513bc4173366ce
Amol Dalvi <amol@imeritize.com>
2014-07-06 09:16:32 -0400

    Reminder emails enhancements
    Make prop reminder emails html safe. Show from name in vote reminder
    emails. Show from name and email in props reminder emails.

--- app/mailers/prop_mailer.rb
+++ app/mailers/prop_mailer.rb
@@ -5,19 +5,19 @@ class PropMailer < ActionMailer::Base
   def prop_mail(user,prop)
   	@user = user
   	@prop = prop
-    mail :to => user.email, :subject => prop.subject
+    mail :to => user.email, :subject => prop.subject, :from => prop.name + '<' + prop.email + '>'
   end
 
   def prop_mail_reminder2(user,prop)
   	@user = user
   	@prop = prop
-    mail :to => user.email, :subject => prop.subject2
+    mail :to => user.email, :subject => prop.subject2, :from => prop.email + '<' + prop.email + '>'
   end
 
   def prop_mail_reminder3(user,prop)
   	@user = user
   	@prop = prop
-    mail :to => user.email, :subject => prop.subject3
+    mail :to => user.email, :subject => prop.subject3, :from => prop.email + '<' + prop.email + '>'
   end
   def prop_notification_email(prop_display)
     
--- app/mailers/vote_mailer.rb
+++ app/mailers/vote_mailer.rb
@@ -4,19 +4,19 @@ class VoteMailer < ActionMailer::Base
   def vote_mail(user,vote_setting)
   	@user = user
   	@vote_setting = vote_setting
-    mail :from => vote_setting.email_sender_email,:to => user.email, :subject => vote_setting.email_sender_subject1
+    mail :from => vote_setting.email_sender_name + '<' + vote_setting.email_sender_email + '>',:to => user.email, :subject => vote_setting.email_sender_subject1
   end
 
   def vote_mail_reminder2(user,vote_setting)
   	@user = user
   	@vote_setting = vote_setting
-    mail :from => vote_setting.email_sender_email,:to => user.email, :subject => vote_setting.email_sender_subject2
+    mail :from => vote_setting.email_sender_name + '<' + vote_setting.email_sender_email + '>',:to => user.email, :subject => vote_setting.email_sender_subject2
   end
 
   def vote_mail_reminder3(user,vote_setting)
   	@user = user
   	@vote_setting = vote_setting
-    mail :from => vote_setting.email_sender_email,:to => user.email, :subject => vote_setting.email_sender_subject3
+    mail :from => vote_setting.email_sender_name + '<' + vote_setting.email_sender_email + '>',:to => user.email, :subject => vote_setting.email_sender_subject3
   end
   def award_selection_email(au,vote_setting)
     @au = au
