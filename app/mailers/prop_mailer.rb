d1105b5a50c372623d2d2f526258b24b888716ea
Amol Dalvi <amol@imeritize.com>
2014-07-06 09:18:44 -0400

    Part of earlier commit
    Show props from name in reminder emails

--- app/mailers/prop_mailer.rb
+++ app/mailers/prop_mailer.rb
@@ -11,13 +11,13 @@ class PropMailer < ActionMailer::Base
   def prop_mail_reminder2(user,prop)
   	@user = user
   	@prop = prop
-    mail :to => user.email, :subject => prop.subject2, :from => prop.email + '<' + prop.email + '>'
+    mail :to => user.email, :subject => prop.subject2, :from => prop.name + '<' + prop.email + '>'
   end
 
   def prop_mail_reminder3(user,prop)
   	@user = user
   	@prop = prop
-    mail :to => user.email, :subject => prop.subject3, :from => prop.email + '<' + prop.email + '>'
+    mail :to => user.email, :subject => prop.subject3, :from => prop.name + '<' + prop.email + '>'
   end
   def prop_notification_email(prop_display)
     
