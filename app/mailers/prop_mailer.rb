43ffb435e30e367afc849e81258503cc9234a3a3
Amol Dalvi <amol@imeritize.com>
2014-07-10 23:36:43 -0400

    Debug why emails aren't getting thru

--- app/mailers/prop_mailer.rb
+++ app/mailers/prop_mailer.rb
@@ -5,12 +5,20 @@ class PropMailer < ActionMailer::Base
   def prop_mail(user,prop)
   	@user = user
   	@prop = prop
+    puts "Mail to: " + user.email
+    puts "Mail subject: " + user.subject
+    puts "Mail from: " + prop.name
+    puts "Mail email:" + prop.email
     mail :to => user.email, :subject => prop.subject, :from => prop.name + '<' + prop.email + '>'
   end
 
   def prop_mail_reminder2(user,prop)
   	@user = user
   	@prop = prop
+    puts "Mail to: " + user.email
+    puts "Mail subject: " + user.subject
+    puts "Mail from: " + prop.name
+    puts "Mail email:" + prop.email
     mail :to => user.email, :subject => prop.subject2, :from => prop.name + '<' + prop.email + '>'
   end
 
