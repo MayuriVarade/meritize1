88eb552f6129769b7f72bee412cfade33cc0bf8c
Amol Dalvi <amol@imeritize.com>
2014-07-20 20:44:23 -0400

    Password reset is a reset, not a new password

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -8,7 +8,7 @@ class PasswordResetsController < ApplicationController
     if user
       
       user.send_password_reset
-      redirect_to password_resets_new_url, :notice => "A new password has been emailed to you. Follow the instructions in the email to start using the new password."
+      redirect_to password_resets_new_url, :notice => "A link to reset your password has been emailed to you. Follow the instructions in the email to set a new password."
     else
       redirect_to password_resets_new_url, :notice => "Sorry, we could not locate a Meritize user with that email address"
     end
@@ -21,7 +21,7 @@ class PasswordResetsController < ApplicationController
   def update
     @user = User.find_by_password_reset_token!(params[:id])
     if @user.password_reset_sent_at < 2.hours.ago
-      redirect_to new_password_reset_path, :alert => "The new password we set for you has expired. You need to request another password."
+      redirect_to new_password_reset_path, :alert => "The link we sent you has expired. You need to submit a reset password link agin."
     elsif @user.update_attributes(params[:user])
       redirect_to root_url, :notice => "Password has been reset! Log in with your new password to continue."
     else
